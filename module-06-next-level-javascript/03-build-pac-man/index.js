fc58105d598ab138ad59d41d4f1e96d8a0a1aa33
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-09-22 02:39:53 +0700

    added layouts using if else statements

--- module-06-next-level-javascript/03-build-pac-man/index.js
+++ module-06-next-level-javascript/03-build-pac-man/index.js
@@ -51,6 +51,16 @@ function createBoard() {
     
     // put square in squares array
     squares.push(square)
+
+    if (layout[i] === 0) {
+      squares[i].classList.add('pac-dot')
+    } else if (layout[i] === 1) {
+      squares[i].classList.add('wall')
+    } else if (layout[i] === 2) {
+      squares[i].classList.add('ghost-lair')
+    } else if (layout[i] === 3) {
+      squares[i].classList.add('power-pellet')
+    } 
   }
 }
 
