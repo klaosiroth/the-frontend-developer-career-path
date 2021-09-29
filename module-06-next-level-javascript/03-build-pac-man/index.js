3bc80a29bc4cf264e5ba0c1f1f1e837f6d521ab1
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-09-29 20:31:09 +0700

    added move the ghosts

--- module-06-next-level-javascript/03-build-pac-man/index.js
+++ module-06-next-level-javascript/03-build-pac-man/index.js
@@ -164,5 +164,36 @@ const ghosts = [
 
 // draw my ghosts onto my grid
 ghosts.forEach(ghost => {
-  squares[ghost.startIndex].classList.add(ghost.className)
+  squares[ghost.currentIndex].classList.add(ghost.className)
+  squares[ghost.currentIndex].classList.add('ghost')
 })
+
+// move the ghosts
+ghosts.forEach(ghost => moveGhost(ghost))
+
+function moveGhost(ghost) {
+  console.log('moved ghost')
+  const directions = [-1, +1, -width, +width]
+  let direction = directions[Math.floor(Math.random() * directions.length)]
+  console.log(direction)
+
+  ghost.timerId = setInterval(function() {
+    // if the next square does NOT contain a wall and does not contain a ghost
+    if (
+      !squares[ghost.currentIndex + direction].classList.contains('wall') &&
+      !squares[ghost.currentIndex + direction].classList.contains('ghost')
+    ) {
+      // remove any ghost
+      squares[ghost.currentIndex].classList.remove(ghost.className)
+      squares[ghost.currentIndex].classList.remove('ghost')
+
+      // add direction to current index
+      ghost.currentIndex += direction
+
+      // add ghost class
+      squares[ghost.currentIndex].classList.add(ghost.className)
+      squares[ghost.currentIndex].classList.add('ghost')
+    } else direction = directions[Math.floor(Math.random() * directions.length)]
+
+  }, ghost.speed)
+}
